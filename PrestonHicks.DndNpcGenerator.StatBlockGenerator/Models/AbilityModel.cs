69a4604ae40cc444e637827e19f0e5cafd9711a3
PrestonHicks26 <prestonhicks26@gmail.com>
2023-11-11 23:41:03 -0500

    - Added StatArray Model - Started implementing models used in statblock generation

---
+++ PrestonHicks.DndNpcGenerator.StatBlockGenerator/Models/AbilityModel.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+
+namespace PH.DndNpcGenerator.StatBlockGenerator.Models
+{
+    public class AbilityModel
+    {
+        private string _name;
+        public string Name { set { _name = value; } get { return _name; } }
+        private string _description;
+        public string Description { set { _description = value; } get { return _description; } }
+        private string _origin;
+        public string Origin { set { _origin = value; } get { return _origin; } }
+        private List<string> _tags;
+        public List<string> Tags {set { _tags = value; } get { return _tags; } }
+        public AbilityModel()
+        {
+
+        }
+        public AbilityModel(StatArrayModel statArray, SettingsModel settings, int points)
+        {
+            // deserialize Abilities.json using list of Ability objects
+            List<Ability> AbilityList;
+            //C:\Users\prest\source\repos\PrestonHicks.DndNpcGenerator\PrestonHicks.DndNpcGenerator.StatBlockGenerator\PH.DndNpcGenerator.StatBlockGenerator.csproj
+            using (StreamReader reader = new StreamReader("..\\PrestonHicks.DndNpcGenerator.StatBlockGenerator\\Data\\Abilities.json"))
+            {
+                var AbilitiesJson = reader.ReadToEnd();
+                AbilityList = JsonConvert.DeserializeObject<List<Ability>>(AbilitiesJson);
+            }
+
+            // create sublist of all valid abilities based on point value and valid tags
+
+            // choose random ability from sublist
+
+            // set properties based on randomly chosen ability
+            Name = AbilityList[0].Name;
+            Description = AbilityList[0].Description;
+            Origin = AbilityList[0].Origin;
+            Tags = AbilityList[0].Tags;
+        }
+        private class Ability
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Origin { get; set; }
+            public int Cost { get; set; }
+            public List<string> Tags { get; set; }
+        }
+    }
+}
+//[
+//  {
+//    "name": "example",
+//    "description": "It does a bunch of cool stuff",
+//    "origin": "src",
+//    "tags": [
+//      "fire",
+//      "leader",
+//      "dwarf"
+//    ]
+//  }
+//]
--- PrestonHicks.DndNpcGenerator.StatBlockGenerator/Models/SettingsModel.cs
+++ PrestonHicks.DndNpcGenerator.StatBlockGenerator/Models/SettingsModel.cs
@@ -2,5 +2,37 @@
 {
     public class SettingsModel
     {
+        private int _challengeRating;
+        private string _ancestry;
+        private string _combatArchetype;
+        private string _abilityStyle;
+        private string _magicType;
+        private bool _usesTraditionalSpellcasting;
+        private string _attackMode;
+        private bool _hasElementalTheme;
+        private string _elementalTheme;
+        private bool _isLeader;
+        private bool _isLegendary;
+        private bool _isActionOriented;
+        private bool _hasMagicItems;
+        private int _magicItemsAmount;
+        private int _magicItemsRarity;
+        public int ChallengeRating { get; set; }
+        public string Ancestry { get; set; }
+        public string CombatArchetype { get; set; }
+        public string AbilityStyle { get; set; }
+        public string MagicType { get; set; }
+        public bool UsesTraditionalSpellcasting { get; set; }
+        public string AttackMode { get; set; }
+        public bool HasElementalTheme { get; set; }
+        public string ElementalTheme { get; set; }
+        public bool IsLeader { get; set; }
+        public bool IsLegendary { get; set; }
+        public bool IsActionOriented { get; set; }
+        public bool HasMagicItems { get; set; }
+        public int MagicItemsAmount { get; set; }
+        public int MagicItemsRarity { get; set; }
+
+
     }
 }
